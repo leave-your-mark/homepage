9ec8127102bcf8fa05efb0fafd07989b32564dd3
Jorge Yau <jyau01@citymail.cuny.edu>
2016-04-04 01:28:28 -0400

    Added node express app skeleton

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # homepage
+
+
+### Installation
+
+```
+# Run the following commands.
+npm install
+
+# Visit your browser at.
+http://localhost:3000
+```
---
+++ app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+
+/********************************************************************
+* CONFIGURATION
+*********************************************************************/
+var app = express();
+
+// Setup bodyParser to let us parse POST data.
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Set the static files path to public.
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set the default view engine to html.
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
+// Specify our default port.
+var PORT = process.env.PORT || 3000;
+
+
+/********************************************************************
+* API ROUTING
+*********************************************************************/
+
+// Register router for api.
+app.use('/', require('./server/pages/router'));
+app.use('/api', require('./server/api/router'));
+
+
+/********************************************************************
+* RUN SERVER
+*********************************************************************/
+app.listen(PORT, function() {
+  console.log('Listening to app on http://localhost:%s ', PORT);
+});
